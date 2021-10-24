133a55876da6e77e4b35b12bf29bce18b9f6e440
maoritz <maorattias17@gmail.com>
2021-10-24 19:38:50 +0300

    User Story 1 task 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Desktop from './components/Desktop'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Desktop />
+      <div className="mobile-app">
+        <img className='logo' src="logo.png" alt="Logo" />
+      </div>
     </div>
   );
 }
