968635b28ce360d59c9b050ebe0c2a449c62d6e3
maoritz <maorattias17@gmail.com>
2021-10-27 00:38:46 +0300

    market value html and design

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,7 +3,14 @@ import './menu.css'
 const Menu = () => {
     return (
         <div className="menu">
-           <img className='logo' src="logo.png" alt="Logo" />
+            <img className='logo' src="logo.png" alt="Logo" />
+            <div className="marketValue">
+                <div>Market is up</div>
+                <div className="percentage-box">
+                    <img className='arrow' src="green_arrow.png" alt="" />
+                    <span>+3.5%</span>
+                </div>
+            </div>
         </div>
         
     )
