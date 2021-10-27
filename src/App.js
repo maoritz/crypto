018a5ef90ec7385e401164eee17e11f3771f5233
maoritz <maorattias17@gmail.com>
2021-10-28 00:19:37 +0300

    us2 task2 conditional rendering to market status

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Desktop from './components/Desktop'
 import Menu from './components/Menu'
+import React, { useState } from 'react'
 import './App.css'
 
 
 function App() {
+
+   const [marketValue, setMarketValue] = useState(7.8)
+
   return (
     <div className="App">
       <Desktop />
-      <Menu />
+      <Menu marketValue={marketValue}/>
     </div>
   );
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,18 +1,20 @@
 import './menu.css'
 
-const Menu = () => {
+const Menu = ({marketValue}) => {
+
+    const marketStatus = (value,up,down) => value > 0 ? up : down
+
     return (
         <div className="menu">
             <img className='logo' src="logo.png" alt="Logo" />
             <div className="marketValue">
-                <div>Market is up</div>
-                <div className="percentage-box">
-                    <img className='arrow' src="green_arrow.png" alt="" />
-                    <span>+3.5%</span>
+                <div>Market is {marketStatus(marketValue,'up','down')}</div>
+                <div className="percentage-box" >
+                    <img className='arrow' src={`${marketStatus(marketValue,'green','red')}_arrow.png`} alt="arrow"  />
+                    <span style={{color:marketStatus(marketValue,'#59bb6e','#DB4848')}}>{marketValue}%</span>
                 </div>
             </div>
         </div>
-        
     )
 }
 
