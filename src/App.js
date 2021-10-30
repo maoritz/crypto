d8d9b120290bb7c8593f4988bd817291c8b29422
maoritz <maorattias17@gmail.com>
2021-10-30 20:11:20 +0300

    us2 task4 get the market change from the last 24 hourse from the API

--- src/App.js
+++ src/App.js
@@ -6,21 +6,32 @@ import './App.css'
 
 
 function App() {
-  const [marketValue, setMarketValue] = useState(7.8)
-   
+  const [marketValue, setMarketValue] = useState()
+
   const axiosOptions = {
     headers: {'user-access-token': '55a9b1f7-d694-463e-9853-7a035952ecf9'}
   };
-  
-  const data = async () => {
-    const response = await  axios.get('https://api.sprintt.co/crypto/', axiosOptions)
-    const data = response.data
-    console.log(data)
+
+   useEffect(() => {
+    const get_current_market_value = async () => {
+      const current_market_value = await fetchMarketData()
+      const value = await Object.values(current_market_value)[0]
+      const cutPercent = await value.split("%")[0]
+      const toNumber = await Number(cutPercent)
+
+      setMarketValue(toNumber)
+    }
+      get_current_market_value()
+  },[])
+
+  // Fetch Data
+  const fetchMarketData = async () => {
+    const response = await fetch(
+      'https://api.sprintt.co/crypto/currencies/market_change',axiosOptions
+    );
+    const data = await response.json()
+    return data
   }
- 
-  data().catch(e => {
-    console.log('There has been a problem with your fetch operation: ' + e.message)
-  })
   
   return (
     <div className="App">
