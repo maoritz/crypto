7290b2141177b62a4979a66d19d42162e6462be1
maoritz <maorattias17@gmail.com>
2021-11-14 00:36:46 +0200

    us3 task 2 - using mock data to display currencies and tracked currencies with local state.

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -9,18 +9,20 @@ const Currency = ({currencyData}) => {
    const {imageSource,textColor} = getMarketStatusFormating(value)
   
     return (
-        <div className='currency'>   
-            <div className="icon-name-symbol-box">
-                <img src={image_url} alt="coin icon" />
-                <div className='name-symbol-box'>
-                    <div>{name}</div>
-                    <div>{symbol}</div>
+        <div className="currency-cotainer">
+            <div className='currency'>   
+                <div className="icon-name-symbol-box">
+                    <img src={image_url} alt="coin icon" />
+                    <div className='name-symbol-box'>
+                        <div>{name}</div>
+                        <div>{symbol}</div>
+                    </div>
                 </div>
-            </div>
-            <div className="rate-box">
-                <div>${price}</div>
-                <div style={{color:textColor}}><img className='arrow' src={imageSource} alt="arrow" />{change_24h}</div>
-            </div>
+                <div className="rate-box">
+                    <div>${price}</div>
+                    <div style={{color:textColor}}><img className='arrow' src={imageSource} alt="arrow" />{change_24h}</div>
+                </div>
+            </div>       
         </div>
     )
 }
--- src/components/TrackedCurrencies.jsx
+++ src/components/TrackedCurrencies.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import {useState} from 'react'
+import Currency from './Currency'
+
 
 const TrackedCurrencies = () => {
+    const [trackedCurrencies,setTrackedCurrencies] = useState([
+        {
+        "change_24h": "-7.07%",
+        "currency_id": 1,
+        "image_url": "http://api.sprintt.co/crypto/currencies/icon_images/aave.jpg",
+        "is_tracked": 1,
+        "name": "Aave",
+        "price": 232.32,
+        "symbol": "AAVE"
+        },
+        {
+        "change_24h": "-4.02%",
+        "currency_id": 14,
+        "image_url": "http://api.sprintt.co/crypto/currencies/icon_images/bitcoin.jpg",
+        "is_tracked": 1,
+        "name": "Bitcoin",
+        "price": 33653,
+        "symbol": "BTC"
+        } 
+    ])
     return (
         <div>
-            TrackedCurrencies
+            {trackedCurrencies.map((currency,index) => {
+               return <Currency currencyData = {trackedCurrencies[index]}/>
+            })}
         </div>
     )
 }
